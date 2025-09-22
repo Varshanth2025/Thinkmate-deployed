623377c641919887b413116d9d0b3563af0be8f6
Varshanth2025 <varshanthg2030@gmail.com>
2025-09-22 15:22:02 +0530

    last commit

--- base/models.py
+++ base/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    name=models.CharField(max_length=200,null=True)
-    email=models.EmailField(unique=True,null=True)
-    bio=models.TextField(null=True,blank=True)
-    avatar=models.ImageField(null=True,default="avatar.svg")
-    USERNAME_FIELD="email"
-    REQUIRED_FIELDS=[]
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True, default="avatar.svg")
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    @property
+    def avatar_url(self):
+        try:
+            return self.avatar.url
+        except ValueError:
+            return "/static/images/avatar.svg"
 
 class Topic(models.Model):
     name=models.TextField(max_length=200)  
